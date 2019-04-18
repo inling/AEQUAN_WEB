ecc27ae43886fed6808f1a5dd74828414ce74842
inling <giraffe.aup@gmail.com>
2019-04-18 19:56:22 +0800

    add nav

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,103 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container>
+      <el-header class="header">
+        <el-row class="logo-row">
+          <el-col :span="24">
+            <router-link to="/">
+              <img src="./assets/img/logo.jpg" alt="">
+            </router-link>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="24">
+            <div class="grid-content bg-purple-dark">
+              <el-row class="nav">
+                <el-col v-for="(item,i) in navList" :key="i" :span="3">
+                  <router-link :to="item.router" v-text="item.tag"  :class="{'nav_active':item.navActive}"></router-link>
+                </el-col>
+              </el-row>
+            </div>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-main  class="main">
+        <router-view/>
+      </el-main>
+      <el-footer  class="footer"></el-footer>
+    </el-container>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data(){
+    return {
+      navList:[
+        {tag:'公司介绍',router:'/',navActive:false},
+        {tag:'系统介绍',router:'/system',navActive:false},
+        {tag:'解决方案',router:'/solution',navActive:false},
+        {tag:'产品介绍',router:'/product',navActive:false},
+        {tag:'应用领域',router:'/area',navActive:false},
+        {tag:'项目案例',router:'/case',navActive:false},
+        {tag:'安e商城',router:''},
+      ]
+    }
+  },
+  created(){
+
+  },
+  mounted(){
+
+  },
+  methods:{
+    crouter(){
+      alert(1)
+      for(var i=0;i<this.navList.length;i++){
+        this.navList[i].navActive=false;
+      }
+      item.navActive=true;
     }
   }
 }
+</script>
+
+<style lang="scss">
+  .header,.main,.footer{
+    padding:0 !important;
+  }
+  .header{
+    text-align: center;
+    .logo-row{
+      width:980px;
+      margin:0 auto;
+      text-align:left;
+      .el-col{
+        margin-top:8px;
+      }
+    }
+    .nav{
+      width:980px;
+      height:100%;
+      line-height: 35px;
+      margin:0 auto;
+      .el-col{
+        cursor: pointer;
+        a{
+          display: inline-block;
+          width:100%;
+          height:100%;
+          color:white;
+          font-size: 14px;
+          &:hover{
+            background:#616369;
+          }
+        }
+      }
+    }
+  }
+
+  .nav_active{
+    background:#D9070A;
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
@@ -11,8 +10,25 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
+  data(){
+    return {
+      navList:[
+        {tag:'公司介绍',router:''},
+        {tag:'系统介绍',router:'/system'},
+        {tag:'解决方案',router:'/solution'},
+        {tag:'产品介绍',router:'/product'},
+        {tag:'应用领域',router:'/area'},
+        {tag:'项目案例',router:'/case'},
+        {tag:'安e商城',router:''},
+      ]
+    }
+  },
   components: {
     HelloWorld
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  
+</style>
